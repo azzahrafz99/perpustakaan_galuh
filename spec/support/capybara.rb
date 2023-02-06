c49f5fd6ffabd57a8b9ac5dde1b5978297079498
Fatimah Az Zahra <azzahrafz@gmail.com>
2023-02-06 16:48:25 +0700

    Fix rubocop

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,6 +1,6 @@
 RSpec.configure do |config|
-  config.before :each, type: :system, js: true do
-    url = "http://#{ENV['SELENIUM_REMOTE_HOST']}:4444/wd/hub"
+  config.before :each, js: true, type: :system do
+    url = "http://#{ENV.fetch('SELENIUM_REMOTE_HOST', nil)}:4444/wd/hub"
 
     driven_by :selenium, using: :chrome, options: {
       browser: :remote,
@@ -9,7 +9,7 @@ RSpec.configure do |config|
     }
 
     Capybara.server_host = `/sbin/ip route|awk '/scope/ { print $9 }'`.strip
-    Capybara.server_port = "43447"
+    Capybara.server_port = '43447'
     session_server       = Capybara.current_session.server
     Capybara.app_host    = "http://#{session_server.host}:#{session_server.port}"
   end
